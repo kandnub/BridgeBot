45b9808ad41dd2e4f1d881ea2c3dbff204788f4a
Kandarp <kandnub@gmail.com>
2020-12-21 22:22:23 +0530

    Update bridgebot.py
    Forward messages as a copy

--- bridgebot.py
+++ bridgebot.py
@@ -33,12 +33,12 @@ basic_filters = filters.group & ~filters.edited & ~filters.service
 
 @client.on_message(filters.chat(group_a) & basic_filters)
 async def group_a_to_group_b(client, event):
-    await client.forward_messages(group_b, event.chat.id, event.message_id)
+    await client.forward_messages(group_b, event.chat.id, event.message_id, as_copy=True)
 
 
 @client.on_message(filters.chat(group_b) & basic_filters)
 async def group_b_to_group_a(client, event):
-    await client.forward_messages(group_a, event.chat.id, event.message_id)
+    await client.forward_messages(group_a, event.chat.id, event.message_id, as_copy=True)
 
 
 if __name__ == "__main__":
